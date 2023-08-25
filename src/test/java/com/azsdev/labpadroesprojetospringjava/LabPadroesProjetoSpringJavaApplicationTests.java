876fea8f371572978709b6f9ab00f3775b2aad0d
afzsilva <afzsilva@gmail.com>
2023-08-25 10:21:15 -0300

    Initial commit

---
+++ src/test/java/com/azsdev/labpadroesprojetospringjava/LabPadroesProjetoSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azsdev.labpadroesprojetospringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
