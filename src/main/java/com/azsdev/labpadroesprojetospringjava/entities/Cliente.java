72954b545875494f848bdfff5a88b5186aed3902
afzsilva <afzsilva@gmail.com>
2023-08-25 11:21:38 -0300

    Adiciona anotations as classes de entidade

--- src/main/java/com/azsdev/labpadroesprojetospringjava/entities/Cliente.java
+++ src/main/java/com/azsdev/labpadroesprojetospringjava/entities/Cliente.java
@@ -1,9 +1,15 @@
 package com.azsdev.labpadroesprojetospringjava.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "tb_cliente")
 public class Cliente {
--- src/main/java/com/azsdev/labpadroesprojetospringjava/entities/Endereco.java
+++ src/main/java/com/azsdev/labpadroesprojetospringjava/entities/Endereco.java
@@ -3,9 +3,15 @@ package com.azsdev.labpadroesprojetospringjava.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "tb_endereco")
 public class Endereco {
