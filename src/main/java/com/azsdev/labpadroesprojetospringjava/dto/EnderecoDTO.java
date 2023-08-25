e0e091f4dabc77140b413e58736e57084ee2533c
afzsilva <afzsilva@gmail.com>
2023-08-25 16:05:35 -0300

    Modifica visibilidade dos atributos para private

--- src/main/java/com/azsdev/labpadroesprojetospringjava/dto/EnderecoDTO.java
+++ src/main/java/com/azsdev/labpadroesprojetospringjava/dto/EnderecoDTO.java
@@ -9,14 +9,14 @@ import lombok.Data;
 @Builder
 public class EnderecoDTO {
 
-    public String cep;
-    public String logradouro;
-    public String complemento;
-    public String bairro;
-    public String localidade;
-    public String uf;
-    public String ibge;
-    public String gia;
-    public String ddd;
-    public String siafi;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 }
