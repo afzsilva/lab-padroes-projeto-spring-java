a1d0b9e33cb86aecc78fa71fbcd36836cf8a5ec6
afzsilva <afzsilva@gmail.com>
2023-08-27 16:59:30 -0300

    Adiciona atributo endereco a classe ClienteDTO

--- src/main/java/com/azsdev/labpadroesprojetospringjava/dto/ClienteDTO.java
+++ src/main/java/com/azsdev/labpadroesprojetospringjava/dto/ClienteDTO.java
@@ -1,15 +1,15 @@
 package com.azsdev.labpadroesprojetospringjava.dto;
 
+import com.azsdev.labpadroesprojetospringjava.entities.Endereco;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class ClienteDTO {
-
     private Long id;
     private String nome;
+    private Endereco endereco;
 }
