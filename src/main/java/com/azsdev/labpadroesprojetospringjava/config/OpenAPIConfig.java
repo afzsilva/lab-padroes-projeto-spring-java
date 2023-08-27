92fd336a98360ad40bc73dd764073c7e065a97d2
afzsilva <afzsilva@gmail.com>
2023-08-27 17:04:53 -0300

    Adiciona e configura Swagger

---
+++ src/main/java/com/azsdev/labpadroesprojetospringjava/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package com.azsdev.labpadroesprojetospringjava.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Clientes")
+                        .description("API CADASTRO CLIENTE COM VIACEP")
+                        .version("1.0.0"));
+    }
+}
