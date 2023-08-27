16dd196ca41f3eaabcf5c7e130252bbb30421d71
afzsilva <afzsilva@gmail.com>
2023-08-27 17:02:58 -0300

    Atualiza implementacao servico via CEP

--- src/main/java/com/azsdev/labpadroesprojetospringjava/service/EnderecoService.java
+++ src/main/java/com/azsdev/labpadroesprojetospringjava/service/EnderecoService.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "viacer", url = "https://viacep.com.br/ws")
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface EnderecoService {
     @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
     EnderecoDTO consultarCep(@PathVariable("cep") String cep);
